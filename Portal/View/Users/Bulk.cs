7754def6d4369120ad431f486945249e97e4275d
MustafaHassaan <MustafaHassaan@users.noreply.github.com>
2024-04-23 01:26:33 +0200

    Add project files.

---
+++ Portal/View/Users/Bulk.cs
@@ -0,0 +1,380 @@
+﻿using Controler;
+using Model;
+using Portal.View.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Portal
+{
+    public partial class Employees : Form
+    {
+        int Charcount = 0;
+        InputLanguage MCL = InputLanguage.CurrentInputLanguage;
+        public static string UNPassing;
+        public static string IDPassing;
+        public Employees()
+        {
+            InitializeComponent();
+            LblName.Text = EmpLogin.UNPassing;
+            LbluserId.Text = EmpLogin.IDPassing;
+            if (SMSText.Text != "")
+            {
+                if (SMSText.Text != "")
+                {
+                    InputLanguage MCL = InputLanguage.CurrentInputLanguage;
+                    LblLang.Text = "Language Is : " + MCL.Culture.EnglishName;
+                    if (LblLang.Text == "Language Is : English (United States)")
+                    {
+                        SMSText.MaxLength = 160;
+                        Charcount = SMSText.Text.Length;
+                        Count.Text = Charcount.ToString() + " " + "/" + " " + "160";
+                        int counttext = Convert.ToInt32(Charcount.ToString());
+                        if (counttext <= 160)
+                        {
+                            MN.Text = "1";
+                        }
+                    }
+                    else
+                    {
+                        SMSText.MaxLength = 70;
+                        Charcount = SMSText.Text.Length;
+                        Count.Text = Charcount.ToString() + " " + "/" + " " + "70";
+                        int counttext = Convert.ToInt32(Charcount.ToString());
+                        if (counttext <= 70)
+                        {
+                            MN.Text = "1";
+                        }
+                    }
+                    BSend.Enabled = true;
+                }
+                else
+                {
+
+                    SMSText.MaxLength = 0;
+                    LblLang.Text = "Language";
+                    Count.Text = "0";
+                    MN.Text = "0";
+                    BSend.Enabled = false;
+                }
+            }
+        }
+        PortalContext Db = new PortalContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private async void BtnLog_Click(object sender, EventArgs e)
+        {
+        }
+        private void Close_Click_1(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                Hide();
+                NI.Visible = true;
+            }
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        private void SMSText_TextChanged(object sender, EventArgs e)
+        {
+            if (SMSText.Text != "")
+            {
+                InputLanguage MCL = InputLanguage.CurrentInputLanguage;
+                LblLang.Text = "Language Is : " + MCL.Culture.EnglishName;
+                if (LblLang.Text == "Language Is : English (United States)")
+                {
+                    SMSText.MaxLength = 160;
+                    Charcount = SMSText.Text.Length;
+                    Count.Text = Charcount.ToString() + " " + "/" + " " + "160";
+                    int counttext = Convert.ToInt32(Charcount.ToString());
+                    if (counttext <= 160)
+                    {
+                        MN.Text = "1";
+                    }
+                }
+                else
+                {
+                    SMSText.MaxLength = 70;
+                    Charcount = SMSText.Text.Length;
+                    Count.Text = Charcount.ToString() + " " + "/" + " " + "70";
+                    int counttext = Convert.ToInt32(Charcount.ToString());
+                    if (counttext <= 70)
+                    {
+                        MN.Text = "1";
+                    }
+                }
+                BSend.Enabled = true;
+            }
+            else
+            {
+
+                SMSText.MaxLength = 0;
+                LblLang.Text = "Language";
+                Count.Text = "0";
+                MN.Text = "0";
+                BSend.Enabled = false;
+            }
+        }
+        private async void BSend_Click(object sender, EventArgs e)
+        {
+            string message = "Are You Shure Want Send Multiple SMS Messages?";
+            string title = "Shure ?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                Microsoft.Office.Interop.Excel.Application XLAPP;
+                Microsoft.Office.Interop.Excel.Workbook XLWB;
+                Microsoft.Office.Interop.Excel.Worksheet XLWS;
+                Microsoft.Office.Interop.Excel.Range XLR;
+
+                int XLRow;
+
+                if (TPath.Text != string.Empty)
+                {
+                    XLAPP = new Microsoft.Office.Interop.Excel.Application();
+                    XLWB = XLAPP.Workbooks.Open(TPath.Text);
+                    XLWS = XLWB.Worksheets["Sheet1"];
+                    XLR = XLWS.UsedRange;
+
+                    for (XLRow = 1; XLRow <= XLR.Rows.Count; XLRow++)
+                    {
+                        //DGV.Rows.Add(XLR.Cells[XLRow, 1].Text, XLR.Cells[XLRow, 2].Text);
+                        Db = new PortalContext();
+                        var Actions = new TransAction();
+                        Actions.SMSPhone = "0" + XLR.Cells[XLRow, 1].Text;
+                        Actions.SMSBody = SMSText.Text;
+                        if (SMSText.RightToLeft == RightToLeft.Yes)
+                        {
+                            Actions.Languge = "Language Is : Arabic (Egypt)";
+                        }
+                        else
+                        {
+                            Actions.Languge = "Language Is : English (United States)";
+                        }
+                        Actions.UserId = long.Parse(LbluserId.Text.ToString());
+                        var T = DateTime.Now.ToLongTimeString();
+                        Actions.Date = DateTime.Now;
+                        Actions.Time = T;
+                        Db.TransActions.Add(Actions);
+                        await Db.SaveChangesAsync();
+                    }
+                    XLWB.Close();
+                    XLAPP.Workbooks.Close();
+                }
+                TPath.Clear();
+                SMSText.Clear();
+                MessageBox.Show("Your Bulk SMS Uploaded Successfully ...", "Successfully");
+            }
+            else
+            {
+                return;
+            }
+        }
+        private void CBUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Header_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void LblName_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void MenueLogin_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void MenueLogout_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void LblLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Phone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Bsave_Click(object sender, EventArgs e)
+        {
+            if (SMSText.Text == "")
+            {
+                MessageBox.Show("The Message Is Empty", "Error");
+            }
+            else
+            {
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void MN_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LblLang_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Count_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Charcter_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void PBody_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void TPhone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void SMSText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string Lang = InputLanguage.CurrentInputLanguage.LayoutName;
+            if (Lang == "Arabic (101)")
+            {
+                SMSText.RightToLeft = RightToLeft.Yes;
+            }
+            else
+            {
+                SMSText.RightToLeft = RightToLeft.No;
+            }
+        }
+        private void Btn_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void PBody_Click(object sender, EventArgs e)
+        {
+        }
+        private void TPhone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        private void History_Click(object sender, EventArgs e)
+        {
+            UNPassing = LblName.Text;
+            IDPassing = LbluserId.Text;
+            var EHF = Application.OpenForms["HistoryBulk"];
+            if ((EHF as HistoryBulk) != null)
+            {
+                if (EHF.WindowState == FormWindowState.Minimized)
+                {
+                    EHF.WindowState = FormWindowState.Normal;
+                }
+                else
+                {
+                    EHF.BringToFront();
+                    //Form is already open
+                }
+            }
+            else
+            {
+                HistoryBulk EHFF = new HistoryBulk();
+                EHFF.Show();
+                // Form is not open
+            }
+        }
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+            foreach (var process in Process.GetProcessesByName("Bulk"))
+            {
+                process.Kill();
+            }
+        }
+        private void maxmizesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Maximized;
+            NI.Visible = false;
+        }
+        private void NI_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void NI_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Show();
+                this.WindowState = FormWindowState.Maximized;
+                NI.Visible = false;
+            }
+            else
+            {
+                CMS.Show();
+            }
+        }
+        private void BImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Filter = "ALL Files|*.*|Excel Files |*.xls;*.xlsx;*.xlsm";
+            OFD.ShowDialog();
+            TPath.Text = OFD.FileName;
+        }
+    }
+}
---
+++ Portal/View/Users/HistoryBulk.cs
@@ -0,0 +1,281 @@
+﻿using Controler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Portal.View.Users
+{
+    public partial class HistoryBulk : Form
+    {
+        int mov;
+        int movx;
+        int movy;
+        PortalContext Db;
+        public HistoryBulk()
+        {
+            InitializeComponent();
+            LblName.Text = Employees.UNPassing;
+            LbluserId.Text = Employees.IDPassing;
+        }
+        private void History_Click(object sender, EventArgs e)
+        {
+        }
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        private void Header_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+        private void Header_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movx, MousePosition.Y - movy);
+            }
+        }
+        private void Header_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movx = e.X;
+            movy = e.Y;
+        }
+        private async void EmpHistories_Load(object sender, EventArgs e)
+        {
+            DTF.Value = DateTime.Today;
+            DTT.Value = DateTime.Today;
+            try
+            {
+                GHLT();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Server Is Dowen ...","Error");
+            }
+        }
+
+        //GHLT : Get History List For Today
+        public void GHLT()
+        {
+            Db = new PortalContext();
+            int id = Convert.ToInt32(LbluserId.Text);
+            var PDb = from hist in Db.Histories
+                      where hist.Date == DateTime.Today
+                      join USING in Db.Users
+                      on hist.UserId equals USING.Id
+                      where USING.Id == id
+                      select new
+                      {
+                          hist.SMSPhone,
+                          hist.SMSBody,
+                          hist.Languge,
+                          hist.Status,
+                          hist.Result,
+                          hist.Date,
+                          hist.Time
+                      };
+            DGV.DataSource = PDb.ToList();
+        }
+
+        //GHLT : Get History List For Date Choose
+        public void GHLD()
+        {
+            Db = new PortalContext();
+            int id = Convert.ToInt32(LbluserId.Text);
+            var PDb = from hist in Db.Histories
+                      where hist.Date >= DTF.Value && hist.Date <= DTT.Value
+                      join USING in Db.Users
+                      on hist.UserId equals USING.Id
+                      where USING.Id == id
+                      select new
+                      {
+                          hist.SMSPhone,
+                          hist.SMSBody,
+                          hist.Languge,
+                          hist.Status,
+                          hist.Result,
+                          hist.Date,
+                          hist.Time
+                      };
+            DGV.DataSource = PDb.ToList();
+        }
+
+        //GHLT : Get History Phone List For Today
+        public void GHPT()
+        {
+            Db = new PortalContext();
+            int id = Convert.ToInt32(LbluserId.Text);
+            var PDb = from hist in Db.Histories
+                      where hist.Date == DateTime.Today &&
+                            hist.SMSPhone.Contains(TSerching.Text)
+                      join USING in Db.Users
+                      on hist.UserId equals USING.Id
+                      where USING.Id == id
+                      select new
+                      {
+                          hist.SMSPhone,
+                          hist.SMSBody,
+                          hist.Languge,
+                          hist.Status,
+                          hist.Result,
+                          hist.Date,
+                          hist.Time
+                      };
+            DGV.DataSource = PDb.ToList();
+        }
+
+        //GHLT : Get History Phone List For Date Choose
+        public void GHPD()
+        {
+            Db = new PortalContext();
+            int id = Convert.ToInt32(LbluserId.Text);
+            var PDb = from hist in Db.Histories
+                      where hist.Date >= DTF.Value && hist.Date <= DTT.Value &&
+                            hist.SMSPhone.Contains(TSerching.Text)
+                      join USING in Db.Users
+                      on hist.UserId equals USING.Id
+                      where USING.Id == id
+                      select new
+                      {
+                          hist.SMSPhone,
+                          hist.SMSBody,
+                          hist.Languge,
+                          hist.Status,
+                          hist.Result,
+                          hist.Date,
+                          hist.Time
+                      };
+            DGV.DataSource = PDb.ToList();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TSerching.Text == "" && DTF.Value == DateTime.Today && DTT.Value == DateTime.Today)
+            {
+                GHLT();
+            }
+            if (TSerching.Text == "" && DTF.Value <= DateTime.Today && DTT.Value <= DateTime.Today)
+            {
+                GHLD();
+            }
+            if (TSerching.Text != "" && DTF.Value == DateTime.Today && DTT.Value == DateTime.Today)
+            {
+                GHPT();
+            }
+            if (TSerching.Text != "" && DTF.Value <= DateTime.Today && DTT.Value <= DateTime.Today)
+            {
+                GHPD();
+            }
+        }
+        private void DTF_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+        private void DTT_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+        private void DTF_ValueChanged(object sender, EventArgs e)
+        {
+        }
+        private void DTT_ValueChanged(object sender, EventArgs e)
+        {
+        }
+        private void TSerching_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Key Pres Number Only
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+        private void TSerching_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (TSerching.Text == "" && DTF.Value == DateTime.Today && DTT.Value == DateTime.Today)
+                {
+                    GHLT();
+                }
+                if (TSerching.Text == "" && DTF.Value <= DateTime.Today && DTT.Value <= DateTime.Today)
+                {
+                    GHLD();
+                }
+                if (TSerching.Text != "" && DTF.Value == DateTime.Today && DTT.Value == DateTime.Today)
+                {
+                    GHPT();
+                }
+                if (TSerching.Text != "" && DTF.Value <= DateTime.Today && DTT.Value <= DateTime.Today)
+                {
+                    GHPD();
+                }
+            }
+        }
+        private void Export_Click(object sender, EventArgs e)
+        {
+
+            // creating Excel Application  
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            // creating new WorkBook within Excel application  
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            // creating new Excelsheet in workbook  
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            //// see the excel sheet behind the program  
+            //app.Visible = true;
+            // get the reference of first sheet. By default its name is Sheet1.  
+            // store its reference to worksheet  
+            worksheet = workbook.Sheets["Sheet1"];
+            worksheet = workbook.ActiveSheet;
+            // changing the name of active sheet  
+            worksheet.Name = "Exported From Bulk";
+            // storing header part in Excel  
+            for (int i = 1; i < DGV.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = DGV.Columns[i - 1].HeaderText;
+            }
+            // storing Each row and column value to excel sheet  
+            for (int i = 0; i < DGV.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < DGV.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = DGV.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            var ComputerName = Environment.MachineName;
+            SaveFileDialog SFD = new SaveFileDialog();
+            SFD.InitialDirectory = @"C:\" + ComputerName;
+            SFD.RestoreDirectory = true;
+            SFD.FileName = ".xlsx";
+            SFD.DefaultExt = "xlsx";
+            SFD.Filter = "Excel Files (*.xlsx)|*.xlsx";
+            string FP = "";
+            if (SFD.ShowDialog() == DialogResult.OK)
+            {
+                FP = SFD.FileName;
+            }
+            var x = FP;
+            // save the application  
+            workbook.SaveAs(x, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            // Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue
+            workbook.Close(true, Type.Missing, Type.Missing);
+
+            // Exit from the application  
+            app.Quit();
+        }
+    }
+}
